1b97b0161e20728f05d37b230fcb32b085f7300f
anlErdem <110909869+anlerdem@users.noreply.github.com>
2024-01-27 02:21:30 +0300

    Dashboard

--- src/Routes/Routes.tsx
+++ src/Routes/Routes.tsx
@@ -3,15 +3,24 @@ import HomePage from "../Pages/HomePage/HomePage";
 import SearchPage from "../Pages/SearchPage/SearchPage";
 import CompanyPage from "../Pages/CompanyPage/CompanyPage";
 import App from "../App";
+import CompanyProfile from "../Components/CompanyProfile/CompanyProfile";
+import IncomeStatement from "../Components/IncomeStatement/IncomeStatement";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
         children: [
-            {path: "", element: <HomePage />},
-            {path: "search", element: <SearchPage />},
-            {path: "company/:ticker", element: <CompanyPage />},
+            { path: "", element: <HomePage /> },
+            { path: "search", element: <SearchPage /> },
+            {
+                path: "company/:ticker",
+                element: <CompanyPage />,
+                children: [
+                    { path: "company-profile", element: <CompanyProfile /> },
+                    { path: "income-statement", element: <IncomeStatement /> },
+                ],
+            },
         ]
     }
 ])
