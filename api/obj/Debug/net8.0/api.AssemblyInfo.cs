db0f436f261e47db544440d4ea40ad7f0dfe24da
anlErdem <110909869+anlerdem@users.noreply.github.com>
2024-02-10 23:24:50 +0300

    Identity

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1422db0bb1b7594566579ff420bcc461d4ee52b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f46eac5b46670ffad2664f456f428b04c461d8fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
