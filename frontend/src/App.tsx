150faa105d4ae4f89c928a74599b9984580cb5cd
anlErdem <110909869+anlerdem@users.noreply.github.com>
2024-02-26 17:06:17 +0300

    Context Auth JWT Services

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import { Outlet } from 'react-router';
+import "react-toastify/dist/ReactToastify.css";
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import { ToastContainer } from "react-toastify";
 
 
 
@@ -9,6 +11,7 @@ function App() {
     <>
     <Navbar />
     <Outlet />
+    <ToastContainer />
     </>
   );
 }
